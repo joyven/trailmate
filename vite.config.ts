a168446ea906519fd97b690854d831399367689a
joyven <joyven888@163.com>
2026-01-17 12:59:40 +0800

    配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,19 @@ import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "/trailmate/",
+  build: {
+    outDir: "docs",
+    assetsDir: "assets",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
